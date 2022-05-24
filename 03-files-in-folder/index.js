19c625e8858a278247e4c27a6fc1c42240872516
Ekateryna Sholomickaya <shan_lee@tut.by>
2022-05-25 01:33:16 +0300

    fix: convert bite in kb, 03 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,11 +8,9 @@ const filePath = path.join(__dirname, 'secret-folder');
 
 const options = { withFileTypes: true };
 fs.readdir(filePath, options, (err, data) => {
-  // console.log(data);
   for (let i = 0; i < data.length; i++) {
     // if (!data[i].isDirectory())
     if (data[i].isFile()) {
-      // console.log(data[i].name);
       fs.stat(
         path.join(__dirname, 'secret-folder', data[i].name),
         (err, stat) => {
@@ -20,12 +18,12 @@ fs.readdir(filePath, options, (err, data) => {
           else {
             let name = data[i].name;
             let index = name.indexOf('.');
-            // let ext = name.slice(index + 1, name.lenght);
             let ext = path.extname(name).slice(1);
             name = name.slice(0, index);
-            // console.log(ext);
-            // console.log(name);
-            console.log(` ${name} - ${ext} - ${stat.size}kb`);
+            const size = stat.size / 1024;
+            // console.log('size----------', size);
+
+            console.log(` ${name} - ${ext} - ${size}kb`);
           }
         }
       );
