1bb135a248a9208925d3df379f22fdc13c91536f
Ekateryna Sholomickaya <shan_lee@tut.by>
2022-05-24 16:45:18 +0300

    feat: add assets dir

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -15,47 +15,43 @@ fs.stat(pathDir, (err) => {
     fs.rm(pathDir, { recursive: true }, (err) => {
       if (err) throw err;
       addStyles();
+      addAssets();
     });
   }
   // if (err.code === 'ENOENT') {
   //   console.log('Директории project-dist нет');
   // }
-  else addStyles();
+  else {
+    addStyles();
+    addAssets();
+  }
 });
+
+// ------------------- функция добавления стилей -------------------------------
 
 const addStyles = () => {
   fs.mkdir(pathDir, { recursive: true }, (err) => {
     if (err) throw err;
     console.log('Cоздаем директорию');
 
-    const readStyleFile = (file, writeStream) => {
-      console.log('заашли в чтение стилей');
+    // ----------------- чтение стилей для каждого файла css ------------------------
 
+    const readStyleFile = (file, writeStream) => {
       if (file.isFile() && path.extname(file.name) == '.css') {
         const filePath = path.join(pathStyle, file.name);
-        console.log('filePath----------------------', filePath);
 
         const readStream = fs.createReadStream(filePath);
         readStream.on('data', function (chunk) {
-          console.log(
-            'записываем --------------------------------------------'
-          );
-
           writeStream.write(chunk.toString());
         });
       }
     };
 
-    const readDir = (pathStyle) => {
-      console.log('pathStyle ------------------------------------', pathStyle);
+    // --------------- чтение директории styles.css ----------------------------
 
+    const readDir = (pathStyle) => {
       fs.readdir(pathStyle, { withFileTypes: true }, (err, data) => {
         if (err) throw err;
-        // console.log('data --------------------------', data);
-        // console.log('pethProject --------------------------', pethProject);
-        // const now = new Date();
-        // console.log(now);
-
         const writeStream = fs.createWriteStream(pethProject);
 
         data.forEach((name) => {
@@ -66,88 +62,63 @@ const addStyles = () => {
 
     fs.access(pethProject, (err) => {
       if (err) {
-        console.log(
-          'проверяем наличие файла ------------------------------------------'
-        );
         readDir(pathStyle);
       } else {
         fs.rm(pethProject, { withFileTypes: true }, (err) => {
           if (err) throw err;
-          console.log(
-            'удаляем файл стайл ------------------------------------------'
-          );
           readDir(pathStyle);
         });
       }
     });
   });
 };
-//-------------------------- работа со стилями ----------------------------------
-
-// const readStyleFile = (file, writeStream) => {
-//   if (file.isFile() && path.extname(file.name) == '.css') {
-//     const filePath = path.join(pathStyle, file.name);
-//     const readStream = fs.createReadStream(filePath);
-//     readStream.on('data', function (chunk) {
-//       writeStream.write(chunk.toString());
-//     });
-//   }
-// };
-
-// const readDir = (pathStyle) => {
-//   fs.readdir(pathStyle, { withFileTypes: true }, (err, data) => {
-//     if (err) throw err;
-
-//     const writeStream = fs.createWriteStream(pethProject);
-
-//     data.forEach((name) => {
-//       readStyleFile(name, writeStream);
-//     });
-//   });
-// };
-
-// fs.access(pethProject, (err) => {
-//   if (err) {
-//     readDir(pathStyle);
-//   } else {
-//     fs.rm(pethProject, { withFileTypes: true }, (err) => {
-//       if (err) throw err;
-//       readDir(pathStyle);
-//     });
-//   }
-// });
 
 // --------------------------------- работа с папкой assets -------------------------
 
-// const pathFilesDir = path.join(__dirname, 'project-dist', 'assets');
-// const pathCopyDir = path.join(__dirname, 'assets');
+const addAssets = () => {
+  const pathFilesDir = path.join(__dirname, 'assets');
+  const pathCopyDir = path.join(__dirname, 'project-dist', 'assets');
 
-// fs.stat(pathCopyDir, (err) => {
-//   if (!err) {
-//     fs.rm(pathCopyDir, { recursive: true }, (err) => {
-//       if (err) throw err;
-//       copyFiles(pathFilesDir);
-//     });
-//   } else if (err.code === 'ENOENT') {
-//     copyFiles(pathFilesDir);
-//   }
-// });
+  // fs.mkdir(pathCopyDir, { recursive: true }, (err) => {
+  //   if (err) throw err;
+  //   fs.readdir(pathFilesDir, (err, data) => {
+  //     if (err) throw err;
+  //     console.log('data----------', data);
+  //   });
+  // });
 
-// const copyFiles = (pathFilesDir) => {
-//   fs.mkdir(pathCopyDir, { recursive: true }, (err) => {
-//     if (err) throw err;
-//   });
+  fs.stat(pathCopyDir, (err) => {
+    if (!err) {
+      fs.rm(pathCopyDir, { recursive: true }, (err) => {
+        if (err) throw err;
+        copyFiles(pathFilesDir);
+      });
+    } /*if (err.code === 'ENOENT')*/ else {
+      copyFiles(pathFilesDir);
+    }
+  });
 
-//   fs.readdir(pathFilesDir, (err, data) => {
-//     if (err) throw err;
-//     data.forEach((name) => {
-//       const pathFile = path.join(pathFilesDir, name);
-//       const newPathFile = path.join(pathCopyDir, name);
+  const copyFiles = (pathFilesDir) => {
+    fs.mkdir(pathCopyDir, { recursive: true }, (err) => {
+      if (err) throw err;
+    });
 
-//       fs.copyFile(pathFile, newPathFile, (err) => {
-//         if (err) throw err;
-//         console.log(`Файл ${name} успешно скопирован`);
-//       });
-//     });
-//   });
-// };
+    // fs.readdir(pathFilesDir, (err, data) => {
+    //   if (err) throw err;
+    //   console.log(data);
+    // });
+
+    fs.readdir(pathFilesDir, (err, data) => {
+      if (err) throw err;
+      data.forEach((name) => {
+        const pathFile = path.join(pathFilesDir, name);
+        const newPathFile = path.join(pathCopyDir, name);
+
+        fs.copyFile(pathFile, newPathFile, (err) => {
+          if (err) throw err;
+          console.log(`Файл ${name} успешно скопирован`);
+        });
+      });
+    });
+  };
+};
